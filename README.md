b9b2634fcf6dc242468529bbe83d0a3291474d45
J. Eduardo <j.eduardo@gmail.com>
2017-08-20 14:55:32 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,73 @@
+# Flask demo app
+
+This is a small demo app created in Flask created to exercise automated deployments
+of non-trivial applications.
+
+* Configuration based on environment variables
+* Database-backed entries
+* JSON API
+* States
+* Support for X-Request-ID header for request tracking
+* Logging with JSON to stdout
+
+## Model
+
+The only model defined for the application is the model of an `Entry`.
+
+An `Entry` is composed by the following fields:
+	* id: unique identifier for an entry. This field is generated automatically 
+	and	cannot be modified.
+	* description: a description for the `Entry`. 
+	* comment: a comment over the `Entry`. 
+
+## Configuration
+
+The following environment variables can be configured:
+
+* APP_DATABASE_URI (mandatory): points to the backing database.
+* APP_LOG_LEVEL (optional): allows configuring the application log level. If it is
+not specified, the default log level will be INFO.
+* APP_HOST (optional): specifies which IP the application should bind to. If not
+specified, it will bind to all addresses. T
+* APP_PORT (optional): specified which port the application should bind to. If not
+specified, it will bind to 5000.
+
+## Deployment
+
+The requirements for the application are listed in `requirements.txt`.
+
+## API
+
+The following endpoints are offered:
+
+* /api/v1/entries
+	* parameters: none
+	* methods: GET
+	* description: list all entries in the database
+	* accepts: all formats
+	* returns: application/json
+
+* /api/v1/entries
+	* parameters: JSON object representing an `Entry`
+	* methods: POST
+	* description: create a new entry
+	* accepts: application/json
+	* returns: application/json
+
+* /api/v1/entries/<id>
+	* parameters: JSON object representing an `Entry`
+	* methods: POST
+	* description: update an existing `Entry`
+	* accepts: application/json
+	* returns: application/json
+
+* /api/v1/entries/<id>
+	* parameters: entry id
+	* methods: DELETE
+	* description: removes an entry from the database
+	* accepts: all formats
+	* returns: application/json
+
+# License
+
+This code is licensed under the MIT License.
---
+++ app.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+
+import logging
+import logmatic
+import os
+import sys
+
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from requestid import requestid, RequestIdFilter
+
+
+log_level = logging._nameToLevel[os.environ.get('APP_LOG_LEVEL', 'INFO')]
+# Configure JSON filesystem log handler
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(logmatic.JsonFormatter())
+handler.addFilter(RequestIdFilter())
+# Configure global logger
+logger = logging.getLogger()
+logger.addHandler(handler)
+logger.setLevel(log_level)
+# Create app and database session
+app = Flask('flask-demo-app')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('APP_DATABASE_URI')
+db = SQLAlchemy(app)
+
+
+class Entry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(80), unique=False)
+    comment = db.Column(db.String(120), unique=False)
+
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'description': self.description,
+            'comment': self.comment
+        }
+
+
+@app.before_first_request
+def setup():
+    # Configure application logger
+    app.logger.addHandler(handler)
+    app.logger.setLevel(log_level)
+    # Bootstrapping database schema
+    db.create_all()
+
+
+@requestid
+@app.route('/api/v1/entries', methods=['GET'])
+def list_entries():
+    entries = list(map(lambda x: x.as_dict(), Entry.query.all()))
+    app.logger.info('Retrieved %d entries' % len(entries))
+    return make_response(jsonify(entries), 200)
+
+
+@requestid
+@app.route('/api/v1/entries/<id>', methods=['GET'])
+def list_entry(id):
+    entry = Entry.query.get(id)
+    if entry:
+        app.logger.info('Entry found for id %s: %s' % (id, entry.as_dict()))
+        return make_response(jsonify(entry.as_dict()), 200)
+    else:
+        app.logger.info('Entry not found for id %s' % id)
+        return make_response(jsonify(''), 404)
+
+
+@requestid
+@app.route('/api/v1/entries', methods=['POST'])
+def add_entry():
+    content = request.json
+    entry = Entry()
+    entry.description = content['description']
+    entry.comment = content['comment']
+    db.session.add(entry)
+    db.session.commit()
+    app.logger.info('Added entry: %s' % entry.as_dict())
+    return make_response(jsonify(entry.as_dict()), 200)
+
+
+@requestid
+@app.route('/api/v1/entries/<id>', methods=['POST'])
+def update_entry(id):
+    content = request.json
+    content.pop('id', None)
+    db.session.query(Entry).filter_by(id=id).update(content)
+    entry = Entry.query.get(id)
+    if entry:
+        db.session.commit()
+        app.logger.info('Updated entry %s with data: %s' % (id, content))
+        return make_response(jsonify(entry.as_dict()), 200)
+    else:
+        app.logger.info('Entry not found for id %s' % id)
+        return make_response(jsonify(''), 404)
+
+
+@requestid
+@app.route('/api/v1/entries/<id>', methods=['DELETE'])
+def remove_entry(id):
+    entry = Entry.query.get(id)
+    if entry:
+        db.session.delete(entry)
+        db.session.commit()
+        app.logger.info('Removed entry: %s', entry.as_dict())
+        return make_response(jsonify(entry.as_dict()), 200)
+    else:
+        app.logger.info('Entry not found for id %s' % id)
+        return make_response(jsonify(''), 404)
+
+
+@requestid
+@app.errorhandler(500)
+def internal_error(exception):
+    app.logger.error(exception)
+    return make_response(jsonify(exception), 500)
+
+
+if __name__ == "__main__":
+    host = os.environ.get('APP_HOST', '0.0.0.0')
+    port = os.environ.get('APP_PORT', 5000)
+    app.run(host=host, port=port)
